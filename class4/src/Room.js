95e9041c7411da06c5adb864e99b71305c1742c0
Talha Arman <30651385+TalhaArman@users.noreply.github.com>
2020-08-02 07:41:28 +0500

    change backgound color
    change backgound color using toggle button

--- class4/src/Room.js
+++ class4/src/Room.js
@@ -1,12 +1,17 @@
 import React , {useState} from 'react'
+import './room.css'
 
 
 const Room = ()=> {
     let [isLit , setLit] = useState(true)
     let [temp , setTemp] = useState(70)
     return(
-        <div>
+        <div className={`room ${isLit ? 'lit' : 'dark'}` }>
           <h1>  this room is {isLit ? 'lit' : 'dark'} </h1>
+          <button onClick={()=>setLit(!isLit)}>change background</button>
+         
+
+          
     <h1>temperature = {temp}</h1>
           <button onClick={()=>setTemp(++temp)}> Increase temperature</button>
           <button onClick={()=>setTemp(--temp)}> Increase temperature</button>
